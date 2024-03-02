2f4c385fddf5a913991bec018a1828f50c5368c4
Sulaimanovuran <sulaimanovuran@gmail.com>
2024-03-02 23:12:47 +0600

    db

--- crm/settings.py
+++ crm/settings.py
@@ -79,10 +79,11 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite',
-        'NAME': 'database.db'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
